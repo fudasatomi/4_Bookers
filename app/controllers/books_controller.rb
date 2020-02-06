c4e6f274aa33de07a8c5cb6096bba483bea37f85
fudasatomi <imotas.aduf@gmail.com>
2020-02-06 15:46:52 +0900

    top&index&show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,36 @@ class BooksController < ApplicationController
   end
 
   def index
+  	@books = Book.all
+
+  	@book = Book.new
   end
 
   def show
+  	@book = Book.find(params[:id])
+  end
+
+  def create
+  	book = Book.new(book_params)
+  	book.save
+  	redirect_to books_path
   end
 
   def edit
+  	@book = Book.find(params[:id])
   end
+
+  def update
+
+  end
+
+  def destloy
+
+  end
+
+    private
+  	def book_params
+	  params.require(:book).permit(:title, :body)
+	end
+
 end
