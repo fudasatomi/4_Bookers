04b6468e031ec4439640c07def50ad0ef62a9d4e
fudasatomi <imotas.aduf@gmail.com>
2020-02-08 14:37:41 +0900

    css

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,9 +13,9 @@ class BooksController < ApplicationController
   end
 
   def create
-  	book = Book.new(book_params)
-  	book.save
-  	redirect_to books_path
+ 	book = Book.new(book_params)
+    book.save
+    redirect_to books_path
   end
 
   def edit
